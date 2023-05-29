b1ae664569ab7a1b540fdd55f3f370af6abb3e76
GustavoGuerato <guerato.gustavo@gmail.com>
2023-05-29 18:22:56 -0300

    add of the logo

--- src/App.js
+++ src/App.js
@@ -4,15 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>ESSE SERA UM CLONE DO INSTAGRAM</h1>
-
-
-
       <div className='app_header'>
         <img 
         className='app_headerImage' 
         src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png' 
         alt=''/>
+         <h1>ESSE SERA UM CLONE DO INSTAGRAM</h1>
       </div>
     </div>
   );
